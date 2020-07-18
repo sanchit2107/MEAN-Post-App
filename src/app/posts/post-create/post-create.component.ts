66ccb97248701087959ab752173ba7d94787f08d
sanchit2107 <sanchit2107@gmail.com>
2020-07-18 21:19:00 +0530

    Post- Create Post Component | Listening on event

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html'
+})
+export class PostCreateComponent {
+  onAddPost() {
+    alert("Post Added!!")
+  }
+}
