ac41c03a1be42278c902c556ac347303ea65f403
sanchit2107 <sanchit2107@gmail.com>
2020-07-20 03:34:51 +0530

    Adding forms | Chaneg two way binding

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 
 import { Post } from '../post.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-post-create',
@@ -13,10 +14,13 @@ export class PostCreateComponent {
   enteredContent = '';
   @Output() postCreated = new EventEmitter();
 
-  onAddPost() {
+  onAddPost(form: NgForm) {
+    if(form.invalid) {
+      return;
+    }
     const post: Post = {
-      title: this.enteredTitle,
-      content: this.enteredContent
+      title: form.value.title,
+      content: form.value.content
     };
 
     this.postCreated.emit(post);
