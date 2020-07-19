bdc1d4c12bef6e8e73cdf9976b24bb933cfa0938
sanchit2107 <sanchit2107@gmail.com>
2020-07-20 02:57:53 +0530

    updating header Component | Format Directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component'
 import { from } from 'rxjs';
-import { HeaderComponent } from './header/header/header.component';
+import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 
 @NgModule({
