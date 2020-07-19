0ce8588bb258ac91bb31c75bdbcfe761be662b1c
sanchit2107 <sanchit2107@gmail.com>
2020-07-20 01:53:31 +0530

    Outputing the content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,21 @@ import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component'
 import { from } from 'rxjs';
 import { HeaderComponent } from './header/header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent,
-    HeaderComponent
+    HeaderComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
@@ -27,7 +30,8 @@ import { HeaderComponent } from './header/header/header.component';
     MatInputModule,
     MatCardModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  posts = [
+    {title: "First Post", content: "Here, I am inside the first post'\s content!!"},
+    {title: "Second Post", content: "Here, I am inside the second post'\s content!!"},
+    {title: "Third Post", content: "Here, I am inside the third post'\s content!!"}
+  ]
+
+}
