363ba91dfbb6f93a2f90d1ceaa8364d565691a67
sanchit2107 <sanchit2107@gmail.com>
2020-07-25 02:17:15 +0530

    Adding the Express backend

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-  res.end('Here we are setting up Node Backend Server!!');
-});
+const app = require('./backend/app');
 
-server.listen(process.env.PORT || 3000);
+const port = process.env.PORT || 3000;
+
+app.set('port', port);
+const server = http.createServer(app);
+
+server.listen(port);
