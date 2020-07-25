c84122d8c6305bbc06bf5c0ebe7652d9a593bd92
sanchit2107 <sanchit2107@gmail.com>
2020-07-25 22:39:02 +0530

    Adding the POST backend part

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Header", "Origin, X-Requested-With, Content-Type, Accept");
   res.setHeader("Access-Control-Allo-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
   next();
+});
+
+app.post('/api/posts', (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: 'Post added sucess!!'
+  });
 });
 
 app.use('/api/posts', (req, res, next) => {
