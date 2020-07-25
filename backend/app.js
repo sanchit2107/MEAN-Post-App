b25c8660de7d824fd203954babc9b23d48ae5ea7
sanchit2107 <sanchit2107@gmail.com>
2020-07-25 22:51:37 +0530

    Adding Angular | Sending Post to Server

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Header", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.setHeader("Access-Control-Allo-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
   next();
 });
@@ -21,7 +21,7 @@ app.post('/api/posts', (req, res, next) => {
   });
 });
 
-app.use('/api/posts', (req, res, next) => {
+app.get('/api/posts', (req, res, next) => {
 
   const posts =[
     {
