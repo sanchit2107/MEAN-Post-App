d2a6cb6553d45c80ea0fb3c6ad2c77c8548ab7ee
sanchit2107 <sanchit2107@gmail.com>
2020-07-27 01:08:04 +0530

    Trasforming Response Data | make id equals _id

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { Post } from './post.model';
-import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -15,9 +16,18 @@ export class PostsService {
   constructor(private http: HttpClient) {}
 
   getPosts() {
-    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
-      .subscribe((postData) => {
-        this.posts = postData.posts;
+    this.http.get<{message: string, posts: any}>('http://localhost:3000/api/posts')
+      .pipe(map((postData) => {
+        return postData.posts.map(post => {
+          return {
+            title: post.title,
+            content: post.content,
+            id: post._id
+          };
+        });
+      }))
+      .subscribe((transformedPost) => {
+        this.posts = transformedPost;
         this.postUpdated.next([...this.posts]);
       });
   }
