346addf48f309594ee9e12656342cd358d9f5d6d
sanchit2107 <sanchit2107@gmail.com>
2020-07-26 21:34:36 +0530

    Adding the Post Instance using Mongoose

--- backend/models/post.js
+++ backend/models/post.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const postSchema = mongoose.Schema({
-  titile: { type: String, required: true },
+  title: { type: String, required: true },
   content: { type: String, required: true }
 });
 
