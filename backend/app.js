cea507386179d94263d87d3a536b61d416beb132
sanchit2107 <sanchit2107@gmail.com>
2020-07-30 23:16:54 +0530

    Imolementing EDIT POST feature

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
   next();
 });
 
@@ -47,6 +47,18 @@ app.get('/api/posts', (req, res, next) => {
         message: 'Post fetches succesfully!!',
         posts: documents
       });
+  });
+});
+
+app.put("api/posts/:id", (req, res, next) => {
+  const post = new Post({
+    _id: req.body.id,
+    title: req.body.title,
+    content: req.body.content
+  });
+  Post.updateOne({_id: req.params.id}, post).then(result => {
+    console.log(result);
+    res.status(200).json({message: 'Update sucessfully!!'});
   });
 });
 
